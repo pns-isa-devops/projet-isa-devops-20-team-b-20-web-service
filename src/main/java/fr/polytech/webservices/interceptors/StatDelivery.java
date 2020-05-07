1cb1e30ddc40da05d82853026ea0269fe6aa525e
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-05-07 21:24:35 +0200

    get the real object  pns-isa-devops/projet-isa-devops-20-team-b-20#137

--- src/main/java/fr/polytech/webservices/interceptors/StatDelivery.java
+++ src/main/java/fr/polytech/webservices/interceptors/StatDelivery.java
@@ -1,25 +1,31 @@
 package fr.polytech.webservices.interceptors;
 
-import fr.polytech.entities.Delivery;
-import statistics.StatisticsCollector;
-import statistics.StatisticsCreator;
+import java.io.Serializable;
+import java.util.GregorianCalendar;
 
 import javax.ejb.EJB;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
-import java.io.Serializable;
-import java.util.GregorianCalendar;
+
+import fr.polytech.entities.Delivery;
+import fr.polytech.warehouse.components.DeliveryModifier;
+import fr.polytech.warehouse.exception.UnknownDeliveryException;
+import statistics.StatisticsCreator;
 
 public class StatDelivery implements Serializable {
 
     private static final long serialVersionUID = -5785967766941270647L;
 
-    @EJB private StatisticsCreator statisticsCreator;
+    @EJB
+    private StatisticsCreator statisticsCreator;
+
+    @EJB
+    private DeliveryModifier deliveryModifier;
 
     @AroundInvoke
     public Object intercept(InvocationContext ctx) throws Exception {
-        Delivery delivery = (Delivery) ctx.getParameters()[0];
-        statisticsCreator.addOccupancy(delivery.getDrone().getDroneId(),new GregorianCalendar(),0.25);
+        Delivery delivery = deliveryModifier.findDelivery((String) ctx.getParameters()[0]);
+        statisticsCreator.addOccupancy(delivery.getDrone().getDroneId(), new GregorianCalendar(), 0.25);
         return ctx.proceed();
     }
 }
