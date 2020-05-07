67d80769a0346ef18e185186e586e094d560ea97
Betsara Marcellin <betsara.marcellin@etu.unice.fr>
2020-05-07 21:52:19 +0200

    pns-isa-devops/projet-isa-devops-20-team-b-20#135 add webmethod confirmInvoicePayment and update wsdl

--- src/main/java/fr/polytech/webservices/InvoiceService.java
+++ src/main/java/fr/polytech/webservices/InvoiceService.java
@@ -6,11 +6,14 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 import fr.polytech.entities.Invoice;
+import fr.polytech.invoice.exceptions.InvoiceNotFoundException;
 
 @WebService(targetNamespace = "http://www.polytech.unice.fr/si/4a/isa/dronedelivery/invoice")
 public interface InvoiceService {
 
-
     @WebMethod
     List<Invoice> getInvoices();
+
+    @WebMethod
+    Invoice confirmInvoicePayment(String invoiceID) throws InvoiceNotFoundException;
 }
