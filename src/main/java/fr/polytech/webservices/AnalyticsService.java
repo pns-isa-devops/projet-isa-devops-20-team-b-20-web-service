890f862c74930a943190c9c9925d1c65af4ece8f
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-05-08 19:41:02 +0200

    add an exception pns-isa-devops/projet-isa-devops-20-team-b-20#149

--- src/main/java/fr/polytech/webservices/AnalyticsService.java
+++ src/main/java/fr/polytech/webservices/AnalyticsService.java
@@ -4,15 +4,18 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+import statistics.exception.NoOccupancyOnThatDroneException;
+
 @WebService(targetNamespace = "http://www.polytech.unice.fr/si/4a/isa/dronedelivery/analytics")
 public interface AnalyticsService {
 
     /**
      * Returns the occupancy rate of the drone with droneId
-     * 
+     *
      * @param droneId
+     * @throws NoOccupancyOnThatDroneException
      * @throws Exception
      */
     @WebMethod
-    double getOccupancyRate(@WebParam(name = "drone_id") String droneId);
+    double getOccupancyRate(@WebParam(name = "drone_id") String droneId) throws NoOccupancyOnThatDroneException;
 }
